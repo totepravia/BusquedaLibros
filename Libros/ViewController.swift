0394e4962d39ae1e13acd19f65f1d699fdfd6212
Sara Fernández Menéndez <sarafernandezmenendez@macbook-pro-de-sara.local>
2018-06-03 14:20:57 +0200

    Subida del desarrollo para obtener libros a partir de su código ISBN

--- Libros/ViewController.swift
+++ Libros/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var cajaBusqueda: UITextField!
+    @IBOutlet weak var cajaResultado: UITextView!
+    //8497364678
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func botonLimpiar() {
+        cajaBusqueda.text = ""
+        cajaResultado.text = ""
+    }
+    
+    @IBAction func enviarCodigo() {
+        getLibro()
+    }
+    
+    func getLibro(){
+        let urlFija = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        let url = urlFija + cajaBusqueda.text!
+        let urlLibro = URL(string: url)
+        do {
+            let datos:Data? = try Data(contentsOf: urlLibro!)
+            let texto = String(data:datos!, encoding: String.Encoding.utf8)
+            cajaResultado.text = texto
+        } catch {
+            print("Unable to load data: \(error)")
+        }
+        
+    }
+    
 }
 
